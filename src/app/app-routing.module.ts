f08b9cd7220ccbb69d52f015dcec932310a0e8bb
June-Wu <june.wu.cs@gmail.com>
2021-08-24 01:43:40 -0400

    added default components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 const routes: Routes = [
+  {path:'sidebar', component:SidebarComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { OverviewComponent } from './overview/overview.component';
+import { MarketComponent } from './market/market.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { BankingComponent } from './banking/banking.component';
+import { InvestmentsComponent } from './investments/investments.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    UserProfileComponent,
+    OverviewComponent,
+    MarketComponent,
+    AccountsComponent,
+    BankingComponent,
+    InvestmentsComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
