43c5a179bf6b9a9b1f159cbf5c8cca91bbc8cc18
Illean Zhang <illean@outlook.com>
2021-08-25 14:46:12 +0000

    can get by useid and account id

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -14,25 +14,41 @@ export class AccountsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  accountName: String = ""
-  paramObj = {accountId: 12345}
-  reportData: Account = {
+  //Parameters
+  getAccountByIdParams = {accountId: null}
+  getAccountsByUserParams = {userId: null}
+  
+  //Reported from service
+  reportAccount: Account = {
     accountId: 0,
     userId: 0,
     name: "",
     accountType: "",
     value: 0
-    }
+  }
+  reportAccountList: Account[] = []
 
-  makeServiceCall(){
-    console.log(this.paramObj)
-    this.accountService.getApiData(this.paramObj)
-      .subscribe( (data: Account | undefined)=>{
+  getAccountsByUser(): void{
+    console.log(this.getAccountsByUserParams)
+    this.accountService.getByUser(this.getAccountsByUserParams)
+      .subscribe( (data: Account[])=>{
         console.log(data)
-        if (data !== undefined) {
-          this.reportData = data;
+        if (data.length !== 0) {
+          this.reportAccountList = data;
         }
       } )
   }
+
+  getAccountById(): void{
+    console.log(this.getAccountByIdParams)
+    this.accountService.getById(this.getAccountByIdParams)
+      .subscribe( (data: Account | undefined)=>{
+        console.log(data)
+        if (data !== undefined) {
+          this.reportAccount = data;
+        }
+      } )
+  }
+
 
 }
