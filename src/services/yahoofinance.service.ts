7f078333f40460531ffe0fc86cc68e8cf7ea2787
Illean Zhang <illean@outlook.com>
2021-08-27 11:27:49 +0000

    overview has graph and top gainers/losers

--- src/services/yahoofinance.service.ts
+++ src/services/yahoofinance.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +14,14 @@ export class YahoofinanceService {
         "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
         "x-rapidapi-key": "1a5e3651a7msh74f8051a4f5f966p1d87e8jsn73425a1ad49f"
       }});
-}
+  }
+
+  public getSummary(params = {symbol:'SPY'}){
+    return this.http.get(`https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=${params.symbol}&region=US`,
+      {"headers": {
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        "x-rapidapi-key": "1a5e3651a7msh74f8051a4f5f966p1d87e8jsn73425a1ad49f"
+      }});
+  }
+
 }
